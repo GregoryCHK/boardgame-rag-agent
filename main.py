83b7590e4d54240100a2f456c65beea54b0c5c24
Gregory Champas Kalogirou <grkalogirou@gmail.com>
2025-11-10 16:08:32 +0200

    test llm invokes

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from boardgames-agent!")
+from src.document_processor import DocumentProcessor
+import os
+from pprint import pprint
+
+from src.rag_agent import RAGAgent
+from src.vector_store import VectorStoreManager
+
+# Files directory
+data_dir = os.path.join(os.getcwd(), "rules")
+file_path = os.path.join(data_dir, "codenames.txt")
+
+persist_directory = os.path.join(os.getcwd(), "vector_db")
+os.makedirs(persist_directory, exist_ok=True)
+
+# Initialize the document processor
+processor = DocumentProcessor()
+#documents = processor.process_documents(file_path)
 
 
-if __name__ == "__main__":
-    main()
+# Initialize Chroma manager
+db_manager = VectorStoreManager(persist_directory=persist_directory)
+
+# Create and load vector store
+# db_manager.create_vectorstore(game_name='codenames', documents=documents)
+
+query = "Explain me how to setup monopoly?"
+
+# results = db_manager.similarity_search("codenames", query)
+
+llm = RAGAgent(vector_manager=db_manager)
+
+result = llm.query(game_name="monopoly", question=query)
+
+print(result['answer'])
